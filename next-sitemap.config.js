c6fff69dd975748d3fb53574ee68943e32c79a94
getwellbksc-lab <getwellbksc@gmail.com>
2026-01-09 02:26:26 +0530

    Update next-sitemap.config.js
    Rajeev edit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://getwellbksc.com",
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-    changefreq: "daily",
-    priority: 0.7,
-    generateIndexSitemap: true,
+  siteUrl: "https://getwellbksc.com",
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  sitemapSize: 7000,
+  changefreq: "daily",
+  priority: 0.7,
 };
+
